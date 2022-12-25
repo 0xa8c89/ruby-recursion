41e4d12642e01c09adccb78798d1f2a055acf598
0xa8c89 <117485263+0xa8c89@users.noreply.github.com>
2022-12-25 04:37:56 +0100

    small fixes

--- fib.rb
+++ fib.rb
@@ -1,8 +1,12 @@
+# Fibonacci
+
 def fibs(n)
   r_arr = [0, 1]
   r_arr << r_arr[-1] + r_arr[-2] until r_arr.length.equal?(n)
   r_arr
 end
+
+puts fibs(9).join(' ')
 
 def fibs_rec(n, arr = [0, 1])
   return arr if arr.length == n
@@ -10,5 +14,4 @@ def fibs_rec(n, arr = [0, 1])
   fibs_rec(n, arr << arr[-1] + arr[-2])
 end
 
-puts fibs(9).join(' ')
 puts fibs_rec(9).join(' ')
