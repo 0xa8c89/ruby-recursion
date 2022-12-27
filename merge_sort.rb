59c056283f4e7726620a0b117b864e13dc0401b6
0xa8c89 <117485263+0xa8c89@users.noreply.github.com>
2022-12-27 07:09:15 +0100

    Finish merge sort

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,6 +1,10 @@
 # Merge sort
 
 def merge(arr1, arr2, r_arr = [])
-  arr1[0] < arr2[0] ? r_arr.push(arr1.shift) : r_arr << arr2.shift until arr2.empty? || arr1.empty?
+  arr1[0] < arr2[0] ? r_arr.push(arr1.shift) : r_arr.push(arr2.shift) until arr2.empty? || arr1.empty?
   arr1.empty? ? r_arr.concat(arr2) : r_arr.concat(arr1)
+end
+
+def split(arr)
+  arr.length < 2 ? arr : merge(split(arr.slice!(0, arr.length / 2)), split(arr))
 end
